2d2376fb8d4d795877f47e745644c850cb937397
Peter Mwangi Njoroge <petemaara2@yahoo.com>
2023-09-07 11:15:50 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+/*
+* File: 4-puts.c
+* Author: Peter Njoroge
+*/
+
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
+
