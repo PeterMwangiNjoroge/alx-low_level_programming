aaa5b5d457981ff5a14489515532f22f4be49715
Peter Njoroge <petermaara2@yahoo.com>
2023-09-12 17:27:00 +0300

    There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,17 +4,30 @@
 */
 
 #include "main.h"
-#include <stdio.h>
 
 /**
-* _abs - Computes the absolute value of an integer.
-* @n: The integer to be computed.
-*
-* Return: absolute value of n.
-*/
-int _abs(int n)
+ * print_sign - Prints the sign of a number.
+ * @n: The number of which the sign will be printed.
+ *
+ * Return: 1 if the number is greater than zero,
+ *         0 if the number is zero,
+ *         -1 if the number is less than zero.
+ */
+int print_sign(int n)
 {
-	if (n < 0)
-		return (-n);
-	return (n);
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
