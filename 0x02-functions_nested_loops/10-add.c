505a5373b1ed264b30b867c763a29714a7133c41
Peter Njoroge <petermaara2@yahoo.com>
2023-09-12 20:49:50 +0300

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+/*
+* File: 10-add.c
+* Author: Peter Njoroge
+*/
+
+#include "main.h"
+
+/**
+* add - adds two integers.
+* @a: first number
+* @b: second number
+* Return: int
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
