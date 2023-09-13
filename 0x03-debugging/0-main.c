dc45b8b3e669458f959523a8a4dce82d7824caee
Peter Njoroge <petermaara2@yahoo.com>
2023-09-13 21:58:34 +0300

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,3 @@
-/*
-* File: 0-putchar.c
-* Author: Peter Njoroge
-*/
-
 #include "main.h"
 
 /**
@@ -12,10 +7,10 @@
 
 int main(void)
 {
-		int i;
+	int i;
 
-		i = 0;
-		positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-		return (0);
+	return (0);
 }
