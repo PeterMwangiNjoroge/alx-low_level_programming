069c5bc3864c83309d7d793bcdbf7057965c8188
Peter Njoroge <petermaara2@yahoo.com>
2023-09-14 22:10:17 +0300

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+/*
+* File: 9-fizz_buzz.c
+* Author: Peter Njoroge
+*/
+
+#include "main.h"
+
+/**
+* main -  prints fizz buzz
+* Return: nothing
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%i", i);
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
