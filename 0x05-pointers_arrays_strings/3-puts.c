301c313c0d1a72e031a0395d2a9f5904b149fb29
Peter Njoroge <petermaara2@yahoo.com>
2023-09-18 20:19:14 +0300

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+/*
+* File: 3-puts.c
+* Author: Peter Njoroge
+*/
+
+#include "main.h"
+
+/**
+*  _puts - prints a string, followed by a new line, to stdout
+*  @str: char arg
+*  Return: nothing
+*/
+
+void _puts(char *str)
+{
+	int length = 0;
+
+	while (*str != '\0')
+	{
+		_putchar(str[length]);
+		length++;
+	}
+	_putchar('\n');
+}
