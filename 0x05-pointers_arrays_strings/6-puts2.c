c10e5dfe51ec990a1ad43cbcfce503301b4aa91b
Peter Njoroge <petermaara2@yahoo.com>
2023-09-18 21:21:49 +0300

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+/*
+* File: 6-puts2.c
+* Author: Peter Njoroge
+*/
+
+#include "main.h"
+
+/**
+*  puts2 - prints every other character of a string,
+*  starting with the first character, followed by a new line.
+*  @str: char arg
+*  Return: nothing
+*/
+
+void puts2(char *str)
+{
+	int counter, length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	for (counter = 0; counter <= length; counter += 2)
+	{
+		_putchar(str[counter]);
+	}
+	_putchar('\n');
+}
