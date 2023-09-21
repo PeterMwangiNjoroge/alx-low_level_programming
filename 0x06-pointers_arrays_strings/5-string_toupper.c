e90c23c113324bb83ea590263c625bb57f0fea72
Peter Njoroge <petermaara2@yahoo.com>
2023-09-21 21:47:33 +0300

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,27 @@
+/*
+* File: 5-string_toupper.c
+* Author: Peter Njoroge
+*/
+
+#include "main.h"
+
+/**
+*  string_toupper -  changes string to upper
+*  @s: char arg
+*  Return: char
+*/
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (int i = 0; s[i] != '\0'; i++)
+
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}
