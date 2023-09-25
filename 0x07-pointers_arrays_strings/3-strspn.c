7851a137c90d30fbbcc317dda799e9d57baf72b1
Peter Njoroge <petermaara2@yahoo.com>
2023-09-25 22:06:31 +0300

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,37 @@
+/*
+* File: 3-strspn.c
+* Author: Peter Njoroge
+*/
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+*  _strspn -  gets the length of a prefix substring.
+*  @s: char arg
+*  @accept: char arg
+*  Return: unsigned int
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, k, l;
+
+	k = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		l = 0;
+
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			k++;
+			l = 1;
+		}
+		if (l == 0)
+		{
+			return (k);
+		}
+	}
+	return (0);
+}
