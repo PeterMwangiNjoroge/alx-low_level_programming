0abd4c752c426bad8dfd348ed194454799441a37
Peter Njoroge <petermaara2@yahoo.com>
2023-09-27 16:03:17 +0300

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,25 @@
+/*
+* File: 0-puts_recursion.c
+* Author: Peter Njoroge
+*/
+
+#include "main.h"
+
+/**
+*  _puts_recursion - prints a string, followed by a new line.
+*  @s: char arg
+*  Return: Nothing
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
