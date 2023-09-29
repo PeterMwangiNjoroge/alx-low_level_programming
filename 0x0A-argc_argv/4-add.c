4f9c6a64f58fdb40c57b20f66661218b2208d160
Peter Njoroge <petermaara2@yahoo.com>
2023-09-29 13:35:57 +0300

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,43 @@
+/*
+* File: 4-add.c
+* Author: Peter Njoroge
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*  main - adds positive numbers.
+*  @argc: int arg
+*  @argv: char arg
+*  Return: int
+*/
+
+int main(int argc, char **argv)
+{
+	int i, n, sum = 0;
+	char *flag;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; argv[i]; i++)
+	{
+		n = strtol(argv[i], &flag, 10);
+		if (*flag)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += n;
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
