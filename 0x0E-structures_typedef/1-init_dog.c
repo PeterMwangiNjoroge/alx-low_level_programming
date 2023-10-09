d8a84bd50934ecac1dbdd3915847a0eb1bb35816
Peter Njoroge <petermaara2@yahoo.com>
2023-10-09 23:02:52 +0300

    A dog is the only thing on earth that loves you more than you love yourself

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,26 @@
+/*
+* File: 1-init_dog.c
+* Author: Peter Njoroge
+*/
+
+#include "dog.h"
+
+/**
+ * init_dog - initializes a structure of type dog
+ * @d: pointer to dog structure
+ * @name: pointer to dog name
+ * @age: age of dog
+ * @owner: pointer to dog owner
+ *
+ * Return: nothing
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
