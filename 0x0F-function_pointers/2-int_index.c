2efc1b1113f6104ab2abde1c6266300b0b0a4c71
Peter Njoroge <petermaara2@yahoo.com>
2023-10-11 23:46:34 +0300

    To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,35 @@
+/*
+* File: 2-int_index.c
+* Author: Peter Njoroge
+*/
+
+#include "function_pointers.h"
+
+/**
+  * int_index - searches for an integer.
+  * @array: int
+  * @size: int
+  * @cmp: int pointer
+  *
+  * Return: int
+  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
+
+				i++;
+			}
+		}
+	}
+
+	return (-1);
+}
