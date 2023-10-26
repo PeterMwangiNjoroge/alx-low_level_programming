93ec09b6d9950d976974c4b0bca257f54f3778f3
Peter Njoroge <petermaara2@yahoo.com>
2023-10-26 13:52:11 +0300

    binary_to_uint fix betty error

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,7 +9,6 @@
 * binary_to_uint -  converts a binary number to an unsigned int.
 * @b: is pointing to a string of 0 and 1 chars
 * Return: unsigned int, converted number, or 0
-
 */
 
 unsigned int binary_to_uint(const char *b)
