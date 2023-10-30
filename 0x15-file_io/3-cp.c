f655a17f62cba02fb78cf25c3c99e70823d656cd
Peter Njoroge <petermaara2@yahoo.com>
2023-10-31 01:07:48 +0300

    test

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,17 +1,47 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/stat.h>
-
-#define BUFSIZE 1024
 
 /**
-* error_exit - error printer
-* @code: code
-* @message: message
-* @arg: arg
-* Return: void
+* set_buffer - set buffer size.
+* @file: pointer to file
+* Return: char
+*/
+char *set_buffer(char *file)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (!buffer)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buffer);
+}
+
+/**
+* close_f - close descriptors.
+* @fd: int arg.
+* Return: nothing
+*/
+void close_f(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+/**
+* error_exit - implements error exit
+* @code: int
+* @message: pointer
+* @arg: pointer
+* Return: nothing
 */
 void error_exit(int code, const char *message, const char *arg)
 {
@@ -21,73 +51,50 @@ void error_exit(int code, const char *message, const char *arg)
 }
 
 /**
-* close_t - closes filedescriptor
-* @fd_from: int file descriptor
-* @fd_to: int file descriptor
-* Return: void
-*/
-void close_t(int fd_from, int fd_to)
-{
-	if (close(fd_from) == -1)
-	{
-		char fd_str[20];
-
-		error_exit(100, "Error: Can't close fd %s\n", fd_str);
-	}
-	if (close(fd_to) == -1)
-	{
-		char fd_str[20];
-
-		error_exit(100, "Error: Can't close fd %s\n", fd_str);
-	}
-}
-
-/**
-* main - check the code
+* main - cp file_a file_b
 * @argc: arguments
-* @argv: arguments
-* Return: Always 0.
+* @argv: list of args
+* Return: 0
 */
 int main(int argc, char *argv[])
 {
-	char *file_from, *file_to, buffer[BUFSIZE];
+	int from, to, r, w;
 
-	int fd_from, fd_to, bool_2;
+	char *buffer;
 
-	ssize_t bytes_read, bytes_written;
-
-	file_from = argv[1];
-	file_to = argv[2];
 	if (argc != 3)
 	{
-		error_exit(97, "Usage: cp file_from file_to\n", "");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
-	fd_from = open(file_from, O_RDONLY);
-	if (fd_from == -1)
-	{
-		error_exit(98, "Error: Can't read from file %s\n", file_from);
-	}
-	bool_2 = S_IRUSR | S_IWUSR | S_IRGRP;
 
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, bool_2);
-	if (fd_to == -1)
-	{
-		error_exit(99, "Error: Can't write to %s\n", file_to);
-	}
-	while ((bytes_read = read(fd_from, buffer, BUFSIZE)) > 0)
-	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1)
+	buffer = set_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (from == -1 || r == -1)
 		{
-			error_exit(99, "Error: Can't write to %s\n", file_to);
+			error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
 		}
-	}
-	if (bytes_read == -1)
-	{
-		error_exit(98, "Error: Can't read from file %s\n", file_from);
-	}
-	close_t(fd_from, fd_to);
-	chmod(file_to, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
+		{
+			error_exit(99, "Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+		}
+
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (r > 0);
+
+	free(buffer);
+	close_f(from);
+	close_f(to);
 
 	return (0);
 }
