01608ad440c77801388445ec10ba02eebd7134cd
Peter Njoroge <petermaara2@yahoo.com>
2023-10-30 21:17:13 +0300

    0 fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,48 +13,43 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int descriptor;
-    char *buffer;
-    ssize_t b_w, b_r;
+	int descriptor;
 
-    if (!filename)
-    {
-        return (0);
-    }
+	char *buffer;
 
-    buffer = (char *)malloc(letters);
+	ssize_t b_w, b_r;
 
-    if (!buffer)
-    {
-        close(descriptor);
-        return (0);
-    }
-    
-    descriptor = open(filename, O_RDONLY);
+	if (!filename)
+	{
+		return (0);
+	}
+	descriptor = open(filename, O_RDONLY);
 
-    if (descriptor == -1)
-    {
-        return (0);
-    }
+	if (descriptor == -1)
+	{
+		return (0);
+	}
 
-    b_r = read(descriptor, buffer, letters);
-    if (b_r == -1)
-    {
-        close(descriptor);
-        free(buffer);
-        return (0);
-    }
+	buffer = (char *)malloc(letters);
 
-    b_w = write(STDOUT_FILENO, buffer, b_r);
-    if (b_w == -1 || b_w != b_r)
-    {
-        close(descriptor);
-        free(buffer);
-        return (0);
-    }
+	if (!buffer)
+	{
+		close(descriptor);
+		return (0);
+	}
 
-    close(descriptor);
-    free(buffer);
-    return (b_w);
+	b_r = read(descriptor, buffer, letters);
+
+	b_w = write(STDOUT_FILENO, buffer, b_r);
+	if (b_r == -1 || b_w == -1 || b_w != b_r)
+	{
+		close(descriptor);
+		free(buffer);
+		return (0);
+	}
+
+	close(descriptor);
+	free(buffer);
+	return (b_w);
 
 }
