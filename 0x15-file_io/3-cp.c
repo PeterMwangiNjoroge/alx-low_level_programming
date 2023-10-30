5c1cb250b4273a21f260cffcfc86812e04d5ea59
Peter Njoroge <petermaara2@yahoo.com>
2023-10-31 00:07:30 +0300

    cp clone

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -74,14 +74,14 @@ int main(int argc, char *argv[])
 	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, bool_2);
 	if (fd_to == -1)
 	{
-		error_exit(99, "Error: Can't write to file %s\n", file_to);
+		error_exit(99, "Error: Can't write to %s\n", file_to);
 	}
 	while ((bytes_read = read(fd_from, buffer, BUFSIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written == -1)
 		{
-			error_exit(99, "Error: Can't write to file %s\n", file_to);
+			error_exit(99, "Error: Can't write to %s\n", file_to);
 		}
 	}
 	if (bytes_read == -1)
